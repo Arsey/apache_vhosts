8f67a606929c74d801f80cfe6f86074f683fd580
wongt <wong.teddy@gmail.com>
2015-01-14 22:50:20 -0500

    Added directory resource for docroot if it does not exist

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,13 @@
-# create VirtualHosts
 node['apache_vhosts'].each do |vhost|
-        web_app vhost[:app] do
+       directory "#{vhost['path']}" do
+                action :create
+                recursive true
+        end
+        web_app "#{vhost['app']}" do
                 template 'vhost.erb'
-                docroot vhost[:path]
-                server_name vhost[:server_name]
-                server_email vhost[:server_email]
-                server_alias vhost[:server_alias]
+                docroot "#{vhost['path']}"
+                server_name "#{vhost['server_name']}"
+                server_email "#{vhost['server_email']}"
+                server_alias "#{vhost['server_alias']}"
         end
 end
