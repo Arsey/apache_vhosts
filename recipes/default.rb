18f8e2f6963441bb0c81bdc4837a3fef72b628a6
wongt <wong.teddy@gmail.com>
2015-01-14 22:30:39 -0500

    Changed Key syntax
    The way the keys where called was not being used properly. The vhosts files where rendered empty.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,10 @@
 # create VirtualHosts
-node['apache_vhosts'].each{|vhost| 
-	web_app vhost['app'] do
-		template 'vhost.erb'
-		docroot vhost['path']
-		server_name vhost['server_name']
-		server_email vhost['server_email']
-		server_alias vhost['server_alias']
-	end
-}
+node['apache_vhosts'].each do |vhost|
+        web_app vhost[:app] do
+                template 'vhost.erb'
+                docroot vhost[:path]
+                server_name vhost[:server_name]
+                server_email vhost[:server_email]
+                server_alias vhost[:server_alias]
+        end
+end
